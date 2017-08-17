24d36236ceb0bd6f842d6128d9340611fd3c094c
liangshiquan <liangshiquan@baidu.com>
2017-08-17 15:17:01 +0800

    update for route

---
+++ src/data.js
@@ -0,0 +1,54 @@
+
+export const list = [
+    {
+        "id": 11,
+        "title": "盖聂",
+        "desc": "剑圣,剑圣,还是剑圣",
+        "tagId": 1
+    },
+    {
+        "id": 12,
+        "title": "司马懿",
+        "desc": "忍忍忍",
+        "tagId": 2
+    },
+    {
+        "id": 13,
+        "title": "诸葛亮",
+        "desc": "吊吊吊",
+        "tagId": 2
+    },
+    {
+        "id": 14,
+        "title": "steve Jobs",
+        "desc": "嘎嘎嘎嘎",
+        "tagId": 3
+    },
+    {
+        "id": 15,
+        "title": "Mark",
+        "desc": "哈哈哈哈哈哈",
+        "tagId": 3
+    },
+    {
+        "id": 16,
+        "title": "天明",
+        "desc": "逗逗逗,还是逗",
+        "tagId": 1
+    }
+];
+
+export const tags = [
+    {
+        id: 1,
+        title: '武侠动漫'
+    },
+    {
+        id: 2,
+        title: '三国'
+    },
+    {
+        id: 3,
+        title: 'IT'
+    }
+];
--- src/routes.js
+++ src/routes.js
@@ -1,20 +1,38 @@
 /**
  * @file routes config arr
  * 
- * @author
+ * @author sqliang
  */
 
 import Home from './pages/Home/Home.san';
 import About from './pages/About/About.san';
+import List from './pages/List/List.san';
+import Add from './pages/Add/Add.san';
+import Edit from './pages/Edit/Edit.san';
 
 export default [
     {
         rule: '/',
         Component: Home
     },
-    
     {
         rule: '/about',
         Component: About
+    },
+    {
+        rule: '/list',
+        Component: List
+    },
+    {
+        rule: '/list/tag/:tag_id',
+        Component: List
+    },
+    {
+        rule: '/add',
+        Component: Add
+    },
+    {
+        rule: '/edit/:id',
+        Component: Edit
     }
 ];
