c89f414dcb35a89772b31143e469fed3e1a711ad
liangshiquan <liangshiquan@baidu.com>
2017-08-21 11:49:55 +0800

    update the about

--- src/data.js
+++ src/data.js
@@ -52,3 +52,97 @@ export const tags = [
         title: 'IT'
     }
 ];
+
+export const techWeekly = [
+    {
+        logo: '',
+        name: 'FEX技术周刊',
+        desc: '每周一刊,业界前沿',
+        url: 'http://fex.baidu.com/articles/'
+    },
+    {
+        logo: '',
+        name: 'Alloy周刊',
+        desc: '每周一刊,业界前沿',
+        url: 'http://www.alloyteam.com/alloyshare/weekly/p/latest'
+    }
+];
+
+export const devArgs = [
+    {
+        logo: '',
+        name: 'EFE',
+        desc: '由百度多个遵循统一技术体系的前端团队所组成',
+        url: ''
+    },
+    {
+        logo: '',
+        name: 'FEX',
+        desc: '百度Web前端研发部',
+        url: ''
+    },
+    {
+        logo: '',
+        name: 'AlloyTeam团队',
+        desc: '腾讯Web前端团队',
+        url: ''
+    },
+    {
+        logo: '',
+        name: 'ISUX',
+        desc: '腾讯社交用户体验设计',
+        url: ''
+    },
+    {
+        logo: '',
+        name: '淘宝前端团队（FED）',
+        desc: '用技术为体验提供无可能',
+        url: ''
+    },
+    {
+        logo: '',
+        name: '凹凸实验室',
+        desc: '京东用户体验设计部出品',
+        url: ''
+    },
+    {
+        logo: '',
+        name: '阿里巴巴国际UED团队',
+        desc: '在更新的屏幕里创造出更好的产品和更优的体验',
+        url: ''
+    }
+];
+
+export const devSns = [
+    {
+        logo: '',
+        name: '掘金',
+        desc: '只有高手分享的中文技术社区',
+        url: ''
+    },
+    {
+        logo: '',
+        name: 'Stack Overflow',
+        desc: '编程相关的IT技术问答网站',
+        url: ''
+    },
+    {
+        logo: '',
+        name: 'GitHub',
+        desc: '面向开源及私有软件项目的git托管平台',
+        url: ''
+    },
+    {
+        logo: '',
+        name: 'V2EX',
+        desc: '一个关于分享和探索的地方',
+        url: ''
+    },
+    {
+        logo: '',
+        name: 'CNode',
+        desc: 'Node.js专业中文社区',
+        url: ''
+    }
+];
+
